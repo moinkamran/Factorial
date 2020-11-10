085f8b1797060090222da26c83b9511295dd2eb3
Moin Kamran <moinjava11@gmail.com>
2020-11-11 01:02:21 +0530

    first commit

---
+++ src/Fact.java
@@ -0,0 +1,22 @@
+// Factorial program by for loop
+public class Fact 
+{
+
+
+	
+	public static void main(String[] args)
+	   {
+	    int num=25;
+	    int factorial=1;
+	    for(int i=num ;i>0;i--)
+	         {
+	    	  factorial=factorial*i;
+	    	 }
+	  System.out.println("Factorial is :" + factorial);  
+	    
+		
+		
+		
+	    }
+
+ }
