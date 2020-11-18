32c630005adc3809a8894f65f3c7d60f161f9a6e
Moin Kamran <moinjava11@gmail.com>
2020-11-19 00:41:10 +0530

    testing do while

---
+++ src/ExampleWhilLoop.java
@@ -0,0 +1,31 @@
+public class ExampleWhilLoop {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		int i;
+		int m = 0, n = 0;
+
+		for (i = 0; i>5; i++) {
+		
+			System.out.println("oh my god");
+			
+		}
+		
+		
+		while(m>5){
+			System.out.println("i am while");
+			m++;
+		}
+
+		
+		do{
+			System.out.println("i am do");
+			n++;
+		}while(n>5);
+		
+	}
+
+}
--- src/Fact.java
+++ src/Fact.java
@@ -1,22 +1,21 @@
 // Factorial program by for loop
-public class Fact 
-{
+public class Fact {
 
+	public static void main(String[] args) {
+		int num = 5;
+		int factorial = 1;
+		for (int i = num; i > 0; i--) {
 
-	
-	public static void main(String[] args)
-	   {
-	    int num=25;
-	    int factorial=1;
-	    for(int i=num ;i>0;i--)
-	         {
-	    	  factorial=factorial*i;
-	    	 }
-	  System.out.println("Factorial is :" + factorial);  
-	    System.out.println("This line is added by Mohammad Miyan");  
-		
-	    System.out.println("This line is moin");  
-		
-	    }
+			factorial = factorial * i;
+		}
+		System.out.println("Factorial is :" + factorial);
+		System.out.println("This line is added by Mohammad Miyan");
+		System.out.println("This line is moin");
+		display();
+	}
 
- }
+	public static void display() {
+		System.out.println("i am moin kamran and i am displye method");
+
+	}
+}
